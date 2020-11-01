140fc518981c1537748685d3647ddf6dcd1dacdd
Demis Struiksma <demis@thenextweb.com>
2020-11-01 19:05:57 +0100

    Sortha finished, need to add some stuff

---
+++ components/Card.js
@@ -0,0 +1,25 @@
+import React from 'React'
+import styles from "../styles/Home.module.css"
+
+function Card(props) {
+    return(
+        <div className={styles.card}>
+                <img
+                src={props.image}
+                alt="card-image"
+                className={styles.cardImage}
+                />
+            <div className={styles.cardDescription}>
+                <h2 className={styles.cardHeading}>{props.name}</h2>
+                <h3 className={styles.cardJob}>{props.job}</h3>
+                <h4 className={styles.cardDuration}>{props.duration}</h4>
+                <hr className={styles.cardHr}></hr>
+                <p className={styles.cardText}>{props.description}</p>
+            </div>
+            
+        </div>
+        
+    )   
+}
+
+export default Card
--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,79 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import Card from '../components/Card'
+
+const name = "Demis";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <container>
+        <header className={styles.header}>
+        <img
+          src="./images/profile.jpg"
+          alt="profile-picture"
+          className={styles.headerImage}
+        />
+        <div className={styles.headerDescription}>
+          <h1>{name}</h1>
+          {/* <div className="dot"></div> */}
+          <p>My name is {name}. I'm a web developer from Amsterdam. <br></br>React out to me @ contact@demis.io</p>
+        </div> 
+      </header>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+      <div className={styles.resume}>
+        <h2>Familair with:</h2>
+        <li>- JavaScript</li>
+        <li>- React JS</li>
+        <li>- NextJS</li>
+        <li>- PHP (Laravel)</li>
+        <li>- HTML</li>
+        <li>- CSS</li>
+        <li>- WordPress</li>
+        <li>- CraftCMS</li>
+        <hr></hr>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <h2>I've worked at:</h2>
+        <Card 
+        image="https://via.placeholder.com/200"
+        name="The Next Web (a Financial Times company"
+        job="Web Development Intern"
+        duration="FEB 2020 - NOV 2020" 
+        description="TNW attracts millions of visitors/readers to their tech blog & subbrands. I've been involved in refactoring code, fixing bugs, testing & more. Technologies used: JavaScript, PHP, CSS, Tailwind CSS, HTML , CraftCMS, Docker & Git." />
+        <Card
+         image="https://via.placeholder.com/200"
+         name="SumUp"
+         job="Customer Support"
+         duration="AUG 2019 - FEB 2020"
+         description="At SumUp, I was responsible for helping merchants with issues (troubleshooting). This was done through e-mail and on the phone." />
+        <Card 
+        image="https://via.placeholder.com/200"
+        name="agenZy"
+        job="Developer & Design Intern"
+        duration="APR 2019 - SEPT 2019"
+        description="I have worked on the new website of agenZy. I was responsible for the design and coding of the website. I reported directly to the CEO of the company. I enjoyed working on this project, since the main website of a company is one of the first impressions that potential clients see." />
+        <Card
+        image="https://via.placeholder.com/200"
+        job="Technology Support Specialist"
+        duration="JUL 2018 - DEC 2018"
+        name="Mijndomein" description="During my time at Mijndomein, it was my duty to help (200K+) customers with technical questions/problems." />
+        <Card
+        image="https://via.placeholder.com/200"
+        job="Web Developer (temporarily"
+        duration="APR 2018 - MAY 2018"
+        name="Qnoop" description="I was responsible for building a new webshop that attracts visitors, to buy their sustainable socks. They sell their socks in 11+ countries in Europe. You can see the result: https://qnoop.com. I used WordPress, WooCommerce, and several plugins for this project." />
+        <Card
+        image="https://via.placeholder.com/200"
+        job="Design Intern"
+        duration="NOV 2017" 
+        name="WeTransfer" description="I did a short internship at the design studio of WeTransfer. I have worked on a WeTransfer wallpaper and I have interviewed several WeTransfer employees about their role and experience at WeTransfer (which I then processed in a blog). I directly reported to the Creative Director." />
+      </div>
+      
+      {/* <footer>
+        Demis
+      </footer> */}
+      </container>
       </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
   )
 }
